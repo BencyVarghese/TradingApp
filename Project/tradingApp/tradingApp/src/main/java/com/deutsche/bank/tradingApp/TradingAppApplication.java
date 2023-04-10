b1fae74de4d78cf0321d173a89cd456f29d951b7
Bency Varghese <bency.varun@gmail.com>
2023-04-10 14:38:35 +0200

    Added swagger UI

--- Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/TradingAppApplication.java
+++ Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/TradingAppApplication.java
@@ -3,11 +3,21 @@ package com.deutsche.bank.tradingApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Trading API ",
+                description = "Trading Application REST API",
+                version = "1.0.0"
+        )
+)
 public class TradingAppApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingAppApplication.class, args);
 	}
-
 }
--- Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/controller/TradingAppController.java
+++ Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/controller/TradingAppController.java
@@ -3,17 +3,20 @@ package com.deutsche.bank.tradingApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deutsche.bank.tradingApp.service.SignalHandler;
 
 @RestController
+@RequestMapping("/tradingApp")
 public class TradingAppController {
 
 	@Autowired
 	public SignalHandler signalHandler;
 
-	@GetMapping("/signal/{signal}")
+	@PostMapping("/signal/{signal}")
 	public String callSingnal(@PathVariable int signal) {
 		signalHandler.handleSignal(signal);
 		return "signal is processed successfully ";
