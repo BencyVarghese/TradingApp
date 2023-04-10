78eaa42bba110b814746d22fdec26dd2935421d4
Bency Varghese <bency.varun@gmail.com>
2023-04-10 15:33:50 +0200

    Adding spring security

--- Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/TradingAppApplication.java
+++ Project/tradingApp/tradingApp/src/main/java/com/deutsche/bank/tradingApp/TradingAppApplication.java
@@ -7,15 +7,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Trading API ",
-                description = "Trading Application REST API",
-                version = "1.0.0"
-        )
-)
+@OpenAPIDefinition(info = @Info(title = "Trading API ", description = "Trading Application REST API", version = "1.0.0"))
 public class TradingAppApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingAppApplication.class, args);
